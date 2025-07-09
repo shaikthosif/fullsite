3d146c3129ef155df82ff7dc8f6f4828e020f01f
shaikthosif <thousif9398@gmail.com>
2025-07-09 12:39:06 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+function openlogin(){
+    window.location.href="login.html";
+}
+
+
+// pogo tv:
+// https://i.pinimg.com/736x/4e/6f/84/4e6f84f5e036089eca5dbd45bf7711f3.jpg
+// bg :
+// https://i.pinimg.com/736x/45/dd/5e/45dd5e8ed98086ceb42687e74cc1e861.jpg
